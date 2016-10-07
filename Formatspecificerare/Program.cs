dd3cbf14074b1982b8dc1002178258daeeae886b
linneasellerholm <linneasm@vgy.se>
2016-10-07 15:12:21 +0200

    övning 1 och 2

--- Formatspecificerare/Program.cs
+++ Formatspecificerare/Program.cs
@@ -11,6 +11,15 @@ namespace Formatspecificerare
         static void Main(string[] args)
         {
             //Övning 1
+            DateTime dt = new DateTime(2016, 10, 7, 16, 14, 54, 50);
+            Console.WriteLine(dt.ToString("d"));
+
+            //Övning 2
+            double number = 4*(Math.PI*Math.PI);
+            Console.WriteLine(number.ToString("F4")+"cm2");
+
+            //Övning 3
+
         }
     }
 }
